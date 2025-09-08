8c19f5e612f6fbfbffa1b45d2751f143c864cd56
Héctor Ulises Martha <hectorulisesmarthal477@gmail.com>
2025-09-08 02:13:36 -0700

    Añadiendo proyectos de búsqueda lineal y matrices

---
+++ Busqueda_renglones-columnas_03-09-25/matriz.java
@@ -0,0 +1,37 @@
+public class matriz { 
+    public static void main(String[] args) {
+        int[][] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+        
+        System.out.println("--- Matriz Original ---");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n--- columnas ---");
+        for (int i = 0; i < columnas; i++) {
+            for (int j = 0; j < filas; j++) {
+                System.out.print(matriz[j][i] + " ");
+            }
+        }
+        System.out.println("\n--- filas ---");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+        }
+            System.out.println(); 
+
+
+        System.out.println("\nfin del programa");
+    }
+}
